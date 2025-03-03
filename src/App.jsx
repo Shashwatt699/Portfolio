998d8d86c13d1ffc449dd07117e42ba5a7ed3c18
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-03-03 23:13:17 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css"
 import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+import Home from './components/sections/Home';
+import About from './components/sections/About';
+import Projects from './components/sections/Projects';
 
 function App() {
 
@@ -12,10 +16,14 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div className={`min-h-screen transition-opacity-700 duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100 `}>
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100 `}>
 
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
+      <About />
+      <Projects />
+
       </div>
     </>
   )
